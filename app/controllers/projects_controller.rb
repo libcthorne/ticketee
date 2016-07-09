e559cd4d1ba55b9b82b3175f6bd70e0840af8a3d
Christopher Thorne <libcthorne@gmail.com>
2016-07-09 22:29:09 +0100

    Add changes from chapter 4.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
+    @projects = Project.all
   end
 
   def new
@@ -10,7 +13,7 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
-      flash[:notice] = "Project has been created!" # for next page load (after redirect)
+      flash[:notice] = "Project has been created." # for next page load (after redirect)
       redirect_to @project #, notice: "Project has been created!"
     else
       # If you were to use flash instead of flash.now in this case,
@@ -22,12 +25,38 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been updated."
+      render "edit"
+    end
+  end
+
+  def destroy
+    @project.destroy
+
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
   end
 
   private
 
   def project_params
     params.require(:project).permit(:name, :description)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
   end
 end
